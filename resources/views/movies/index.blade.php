91dc77b87d4a14e6bd1bbde40cb0a5771739ec3c
Michail Spinelli <michailspinelli96@gmail.com>
2024-02-29 16:21:10 +0100

    fine

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('page-title', 'Movies')
+
+@section('main-content')
+<h1>
+    Movies
+</h1>
+
+<div class="row">
+    @foreach ($movies as $movie)
+        <div class="col-12 col-sm-3 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3>
+                        {{ $movie->title }}
+                    </h3>
+
+                    <a href="{{ route('movies.show', ['id' => $movie->id]) }}" class="btn btn-primary">
+                        Film specifico
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+@endsection
